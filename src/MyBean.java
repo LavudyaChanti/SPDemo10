1f36f97010f8fd409ce996099c7be5d1f2921075
chanti <tgtechjava@gmail.com>
2019-03-12 09:53:29 +0530

    'commit2'

--- src/MyBean.java
+++ src/MyBean.java
@@ -1,15 +1,10 @@
 
-public class MyBean 		//OuterBean
-{
-	private SampleBean sb;	//InnerBean
-	//setter
-	public void setSb(SampleBean sb) {
-		this.sb = sb;
-	}
-	//business component
+public class MyBean 		{
+
+	
 	public void saySomeThing()
 	{
-		System.out.println("Data from MyBean:	Welcome Chanti");
-		sb.sayHello();
+		System.out.println("Hi");
+		
 	}
 }
--- src/SampleBean.java
+++
@@ -1,7 +0,0 @@
-public class SampleBean 	
-{
-	public void sayHello()
-	{
-		System.out.println("Data from SampleBean:	Hello Chanti");
-	}
-}
